9d4455b553a8b30b1c288b89b8659440140c23a5
Casper Szymiczek-Graley <casper.szy@gmail.com>
2024-09-29 17:36:22 +1000

    refactor: search functions and add nil checks

--- lua/last-motion/search.lua
+++ lua/last-motion/search.lua
@@ -4,27 +4,31 @@ local M = {}
 
 local state = require("last-motion.state")
 
+local function set_last_search()
+    state.last.search = vim.fn.getreg("/")
+end
+
 --- The same as default behaviour of * and n/N, but in one function
 M.next_search = function()
-    if state.last.search then
+    if state.last and state.last.search then
         -- use the last search, not a new one
         vim.fn.search(state.last.search)
     else
         -- a new search since we have a fresh state.last value
         vim.cmd("normal! *")
-        state.last.search = vim.fn.getreg("/")
+        set_last_search()
     end
 end
 
 --- The same as default behaviour of # and n/N, but in one function
 M.prev_search = function()
-    if state.last.search then
+    if state.last and state.last.search then
         -- use the last search, not a new one
         vim.fn.search(state.last.search, "b")
     else
         -- a new search since we have a fresh state.last value
         vim.cmd("normal! #")
-        state.last.search = vim.fn.getreg("/")
+        set_last_search()
     end
 end
 
@@ -32,14 +36,20 @@ end
 --- So we need a way to get back whatever the most recent search was, and continue through results.
 --- The last count is lost, but you can add count to this keymap.
 M.next_for_recent_search = function()
-    state.last.search = vim.fn.getreg("/")
+    if not state.last then
+        return
+    end
+    set_last_search()
     vim.fn.search(state.last.search)
     vim.opt.hlsearch = true
 end
 
 --- Same as next_for_recent_search, but in reverse direction.
 M.prev_for_recent_search = function()
-    state.last.search = vim.fn.getreg("/")
+    if not state.last then
+        return
+    end
+    set_last_search()
     vim.fn.search(state.last.search, "b")
     vim.opt.hlsearch = true
 end
