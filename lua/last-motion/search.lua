cb97ec8d5696b9c5d6c9e0d60f21d7f04268338c
Casper Szymiczek-Graley <casper.szy@gmail.com>
2024-09-29 19:46:33 +1000

    wip: attach last_search to last, but this keeps this too long. need a way to reset for a new search

--- lua/last-motion/search.lua
+++ lua/last-motion/search.lua
@@ -1,20 +1,29 @@
 -- helpers to reproduce the original search behaviour for / ? * #
+local state = require("last-motion.state")
 
 local M = {
-    --- State which stores the last motion, to be repeated
-    last_search = nil,
     -- FIXME: make this work with history
 }
 
 local function set_last_search()
-    M.last_search = vim.fn.getreg("/")
+    state.last().last_search = vim.fn.getreg("/")
+end
+
+local function find_last_search()
+    for _, value in ipairs(state.history) do
+        if value.last_search then
+            return value.last_search
+        end
+    end
+    return nil
 end
 
 --- The same as default behaviour of * and n/N, but in one function
 M.next_search = function()
-    if M.last_search then
+    local last_search = find_last_search()
+    if last_search then
         -- use the last search, not a new one
-        vim.fn.search(M.last_search)
+        vim.fn.search(last_search)
     else
         -- a new search since we have a fresh state.last value
         vim.cmd("normal! *")
@@ -24,9 +33,10 @@ end
 
 --- The same as default behaviour of # and n/N, but in one function
 M.prev_search = function()
-    if M.last_search then
+    local last_search = find_last_search()
+    if last_search then
         -- use the last search, not a new one
-        vim.fn.search(M.last_search, "b")
+        vim.fn.search(last_search, "b")
     else
         -- a new search since we have a fresh state.last value
         vim.cmd("normal! #")
@@ -38,21 +48,23 @@ end
 --- So we need a way to get back whatever the most recent search was, and continue through results.
 --- The last count is lost, but you can add count to this keymap.
 M.next_for_recent_search = function()
-    if not M.last_search then
+    local last_search = find_last_search()
+    if not last_search then
         return
     end
     set_last_search()
-    vim.fn.search(M.last_search)
+    vim.fn.search(last_search)
     vim.opt.hlsearch = true
 end
 
 --- Same as next_for_recent_search, but in reverse direction.
 M.prev_for_recent_search = function()
-    if not M.last_search then
+    local last_search = find_last_search()
+    if not last_search then
         return
     end
     set_last_search()
-    vim.fn.search(M.last_search, "b")
+    vim.fn.search(last_search, "b")
     vim.opt.hlsearch = true
 end
 
