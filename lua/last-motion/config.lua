1205124b727a982100ef65225191d9909cbfd56a
Casper Szymiczek-Graley <casper.szy@gmail.com>
2024-09-30 18:21:19 +1000

    feat: add treesitter motions, as they are built in to neovim

--- lua/last-motion/config.lua
+++ lua/last-motion/config.lua
@@ -1,4 +1,18 @@
 local search = require("last-motion.search")
+local ts_utils = require("nvim-treesitter.ts_utils")
+local ts_move = require("nvim-treesitter.textobjects.move")
+
+local function ts_next(query)
+    return function()
+        ts_move.goto_next_start(query)
+    end
+end
+
+local function ts_prev(query)
+    return function()
+        ts_move.goto_previous_start(query)
+    end
+end
 
 return {
     max_motions = 10,
@@ -92,6 +106,114 @@ return {
             prev_keys = { "[t" },
             next = vim.cmd.tabnext,
             prev = vim.cmd.tabprevious,
+        },
+
+        -- treesitter functions
+
+        {
+            desc = "[a]ttribute",
+            next_keys = { ",ta" },
+            next = ts_next("@attribute.inner"),
+            prev = ts_prev("@attribute.inner"),
+        },
+
+        {
+            desc = "fra[m]e",
+            next_keys = { ",tm" },
+            next = ts_next("@frame.inner"),
+            prev = ts_prev("@frame.inner"),
+        },
+
+        {
+            desc = "c[o]mment",
+            next_keys = { ",to" },
+            next = ts_next("@comment.outer"),
+            prev = ts_prev("@comment.outer"),
+        },
+
+        {
+            desc = "[b]lock",
+            next_keys = { ",tb" },
+            next = ts_next("@block.inner"),
+            prev = ts_prev("@block.inner"),
+        },
+
+        {
+            desc = "[r]eturn",
+            next_keys = { ",tr" },
+            next = ts_next("@return.inner"),
+            prev = ts_prev("@return.inner"),
+        },
+
+        {
+            desc = "[p]arameter",
+            next_keys = { ",tp" },
+            next = ts_next("@parameter.inner"),
+            prev = ts_prev("@parameter.inner"),
+        },
+
+        {
+            desc = "ca[l]l",
+            next_keys = { ",tl" },
+            next = ts_next("@call.outer"),
+            prev = ts_prev("@call.outer"),
+        },
+
+        {
+            desc = "[a]ssignment",
+            next_keys = { ",ta" },
+            next = ts_next("@assignment.rhs"),
+            prev = ts_prev("@assignment.rhs"),
+        },
+
+        {
+            desc = "con[d]itional",
+            next_keys = { ",td" },
+            next = ts_next("@conditional.inner"),
+            prev = ts_prev("@conditional.inner"),
+        },
+
+        {
+            desc = "[c]lass",
+            next_keys = { ",tc" },
+            next = ts_next("@class.inner"),
+            prev = ts_prev("@class.inner"),
+        },
+
+        {
+            desc = "[f]unction",
+            next_keys = { ",tf" },
+            next = ts_next("@function.outer"),
+            prev = ts_prev("@function.outer"),
+        },
+
+        {
+            desc = "[n]ode",
+            next_keys = { ",tn" },
+            next = function()
+                local node = ts_utils.get_node_at_cursor()
+                if node == nil then
+                    error("No Treesitter parser found.")
+                    return
+                end
+                local next = ts_utils.get_next_node(node, true, false)
+                if next == nil then
+                    -- vim.notify("No next node found.")
+                end
+                ts_utils.goto_node(next, true, false)
+            end,
+            prev = function()
+                local node = ts_utils.get_node_at_cursor()
+                if node == nil then
+                    error("No Treesitter parser found.")
+                    return
+                end
+                local prev = ts_utils.get_previous_node(node, true, false)
+                if prev == nil then
+                    -- vim.notify("No prev node found.")
+                end
+                ts_utils.goto_node(prev, true, false)
+            end,
         },
     },
 }
