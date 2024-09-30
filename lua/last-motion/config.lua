d033ea11b7f1babd1723571c01db632c8be24d33
Casper Szymiczek-Graley <casper.szy@gmail.com>
2024-09-30 22:28:35 +1000

    docs: reorder default config and document different kinds of defs

--- lua/last-motion/config.lua
+++ lua/last-motion/config.lua
@@ -17,55 +17,55 @@ end
 return {
     max_motions = 10,
 
-    -- it doesn't matter which key in a pair is next or prev, as direction is preserved
     definitions = {
-        -- character motions are maybe pointless
-        { next = "h", prev = "l" },
-        { next = "j", prev = "k" },
+        -- Ideally this would have every pair of motions
 
-        -- obvious ones
+        -- it doesn't matter which key in a pair is next or prev, as direction is preserved
+
+        -- with just next and prev, those keys should behave as normal
+        -- as they will be replaced with new keymaps, that just call those keys and remember the motion
+        -- There is no reason to have new keymaps for such basic motions.
         { next = "w", prev = "b" },
         { next = "W", prev = "B" },
         { next = "}", prev = "{" },
         { next = ")", prev = "(" },
+        { next = "e", prev = "ge" },
+        { next = "E", prev = "gE" },
+
+        -- next_key and prev_key: can process control keys too
         { next = "<C-d>", prev = "<C-u>" },
         { next = "<C-f>", prev = "<C-b>" },
         { next = "<C-i>", prev = "<C-o>" },
 
-        -- non obvious ones
-        { next = "e", prev = "ge" },
-        { next = "E", prev = "gE" },
+        -- character motions probably aren't very useful
+        { next = "h", prev = "l" },
+        { next = "j", prev = "k" },
+
+        -- these ones only go back and forth between two positions, so pretty pointless
         { next = "g_", prev = "^" },
         { next = "$", prev = "0" },
         { next = "G", prev = "gg" },
 
-        -- operator pending motions
-        { next = "f", prev = "F", pending = true },
-        { next = "t", prev = "T", pending = true },
-
-        -- default commands, these are a special case as they use command mode
-        { command = "/", next = "n", prev = "N" },
-        { command = "?", next = "n", prev = "N" },
-        -- { command = ":bnext", next = ":bnext", prev = ":bprev" },
-
-        -- some other functions
+        -- next_key and prev_key: when there's existing keys to override
+        -- default keymaps are with [ and ] prefixes, as that's an established pattern in neovim
+        -- New keymaps are added to be more consistent
         {
-            desc = "search",
-            next = "*",
-            prev = "#",
-            next_func = search.next_search,
-            prev_func = search.prev_search,
+            desc = "fo[l]d",
+            next = "]l",
+            prev = "[l",
+            next_key = "zj",
+            prev_key = "zk",
         },
         {
-            -- adds key to get back to recent search results after other movements
-            -- this is needed if you override n and N with repeating motions
-            desc = "recent [s]earch",
-            next = "]s",
-            prev = "[s",
-            next_func = search.next_for_recent_search,
-            prev_func = search.prev_for_recent_search,
+            desc = "[w]indow",
+            next = "]w",
+            prev = "[w",
+            next_key = "<C-w>w",
+            prev_key = "<C-w>W",
         },
 
+        -- next_func and prev_func: when there's a function to call instead of a key
+        -- These need new keymaps, as they don't already ones
         {
             desc = "[d]iagnostic",
             next = "]d",
@@ -75,29 +75,12 @@ return {
         },
 
         {
-            desc = "fo[l]d",
-            next = "]l",
-            prev = "[l",
-            next_key = "zj",
-            prev_key = "zk",
-        },
-
-        {
-            desc = "[w]indow",
-            next = "]w",
-            prev = "[w",
-            next_key = "<C-w>w",
-            prev_key = "<C-w>W",
-        },
-
-        {
             desc = "[q]uickfix item",
             next = "]q",
             prev = "[q",
             next_func = vim.cmd.cnext,
             prev_func = vim.cmd.cprevious,
         },
-
         {
             desc = "[b]uffer",
             next = "]b",
@@ -105,7 +88,6 @@ return {
             next_func = vim.cmd.bnext,
             prev_func = vim.cmd.bprevious,
         },
-
         {
             desc = "[t]ab",
             next = "]t",
@@ -114,8 +96,39 @@ return {
             prev_func = vim.cmd.tabprevious,
         },
 
-        -- treesitter functions
+        -- pending: for operator pending, it will wait until the following key is entered
+        -- maybe it's only a special case for fFtT ?
+        { next = "f", prev = "F", pending = true },
+        { next = "t", prev = "T", pending = true },
 
+        -- search has a few special cases
+        -- command: for commands that are a special case and don't need to create any keymaps
+        { command = "/", next = "n", prev = "N" },
+        { command = "?", next = "n", prev = "N" },
+        -- existing keys, but need to use a new implementation function to deal with starting a new search vs continuing a search
+        -- local search = require("last-motion.search") -- import is required
+        {
+            desc = "search",
+            next = "*",
+            prev = "#",
+            next_func = search.next_search,
+            prev_func = search.prev_search,
+        },
+        -- adds key to get back to recent search results after other movements
+        -- this is needed if you override n and N with repeating motions
+        {
+            desc = "recent [s]earch",
+            next = "]s",
+            prev = "[s",
+            next_func = search.next_for_recent_search,
+            prev_func = search.prev_for_recent_search,
+        },
+
+        -- TODO: can actual command motions be repeated?
+        -- { command = ":bnext", next = ":bnext", prev = ":bprev" },
+
+        -- treesitter functions that are builtin to neovim
+        -- ts_next and ts_prev are defined above
         {
             desc = "[a]ttribute",
             next = "]a",
@@ -123,7 +136,6 @@ return {
             next_func = ts_next("@attribute.inner"),
             prev_func = ts_prev("@attribute.inner"),
         },
-
         {
             desc = "fram[e]",
             next = "]e",
@@ -131,7 +143,6 @@ return {
             next_func = ts_next("@frame.inner"),
             prev_func = ts_prev("@frame.inner"),
         },
-
         {
             desc = "c[o]mment",
             next = "]o",
@@ -139,7 +150,6 @@ return {
             next_func = ts_next("@comment.outer"),
             prev_func = ts_prev("@comment.outer"),
         },
-
         {
             desc = "bloc[k]",
             next = "]k",
@@ -147,7 +157,6 @@ return {
             next_func = ts_next("@block.inner"),
             prev_func = ts_prev("@block.inner"),
         },
-
         {
             desc = "[r]eturn",
             next = "]r",
@@ -155,7 +164,6 @@ return {
             next_func = ts_next("@return.inner"),
             prev_func = ts_prev("@return.inner"),
         },
-
         {
             desc = "[p]arameter",
             next = "]p",
@@ -163,7 +171,6 @@ return {
             next_func = ts_next("@parameter.inner"),
             prev_func = ts_prev("@parameter.inner"),
         },
-
         {
             desc = "[c]all",
             next = "]c",
@@ -171,7 +178,6 @@ return {
             next_func = ts_next("@call.outer"),
             prev_func = ts_prev("@call.outer"),
         },
-
         {
             desc = "[a]ssignment",
             next = "]a",
@@ -179,7 +185,6 @@ return {
             next_func = ts_next("@assignment.rhs"),
             prev_func = ts_prev("@assignment.rhs"),
         },
-
         {
             desc = "co[N]ditional",
             next = "]N",
@@ -187,7 +192,6 @@ return {
             next_func = ts_next("@conditional.inner"),
             prev_func = ts_prev("@conditional.inner"),
         },
-
         {
             desc = "[C]lass",
             next = "]C",
@@ -195,7 +199,6 @@ return {
             next_func = ts_next("@class.inner"),
             prev_func = ts_prev("@class.inner"),
         },
-
         {
             desc = "[f]unction",
             next = "]f",
@@ -204,6 +207,7 @@ return {
             prev_func = ts_prev("@function.outer"),
         },
 
+        -- An attempt at moving through abstract treesitter nodes
         {
             desc = "[n]ode",
             next = "]n",
